4957ea2b25e9f3cd9d1d64d6dfe9087bccaa419b
Michael Bishop <cleverca22@gmail.com>
2024-05-16 13:13:21 -0300

    add virtio-snd config

--- mini-rv32ima/virtio-snd.c
+++ mini-rv32ima/virtio-snd.c
@@ -2,8 +2,24 @@
 #include "virtio.h"
 #include "plic.h"
 
+typedef struct {
+  uint32_t jacks;
+  uint32_t streams;
+  uint32_t chmaps;
+} virtio_snd_config;
+
+static virtio_snd_config cfg = {
+  .jacks = 1,
+  .streams = 1,
+  .chmaps = 1,
+};
+
 static uint32_t virtio_snd_config_load(struct virtio_device *dev, uint32_t offset) {
   uint32_t ret = 0;
+  if (offset < sizeof(cfg)) {
+    uint32_t *cfg32 = (uint32_t*)&cfg;
+    ret = cfg32[offset/4];
+  }
   printf("virtio_snd_config_load(%p, %d) == 0x%x\n", dev, offset, ret);
   return ret;
 }
@@ -13,7 +29,7 @@ static void virtio_snd_config_store(struct virtio_device *dev, uint32_t offset, 
 }
 
 static void virtio_snd_process_command(struct virtio_device *dev, struct virtio_desc_internal *chain, int chain_length, int queue, uint16_t start_idx) {
-  printf("virtio_snd_process_command(%p, %p, %d, %d, %d)\n", dev, chain, chain_length, queue, start_idx);
+  printf("virtio_snd_process_command(%p, %p, length %d, queue %d, %d)\n", dev, chain, chain_length, queue, start_idx);
 }
 
 static const virtio_device_type virtio_snd_type = {
